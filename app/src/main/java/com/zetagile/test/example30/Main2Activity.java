eeb807835e1e9677df62692feb3b13c76693c9ff
image1 <you@example.com>
2017-12-08 11:02:31 +0530

    Initial commit

---
+++ app/src/main/java/com/zetagile/test/example30/Main2Activity.java
@@ -0,0 +1,40 @@
+package com.zetagile.test.example30;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.zetagile.test.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button buttonA1 = (Button)findViewById(R.id.buttonA1);
+
+        buttonA1.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        TextView textViewA1 = (TextView)findViewById(R.id.textViewA1);
+                        textViewA1.setText("New Text Dude");
+                    }
+                }
+        );
+        buttonA1.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                   public boolean onLongClick(View v){
+                       TextView textViewA1 = (TextView)findViewById(R.id.textViewA1);
+                       textViewA1.setText("On Long Press");
+                       return true;
+                   }
+                }
+        );
+    }
+}
